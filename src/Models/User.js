6fe79e16ca6f55fcb32ef0d66c42ee606ef28c6d
Aman Agrawal <amanagrawal318@gmail.com>
2025-07-12 20:05:08 +0530

    added user model

---
+++ src/Models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./Models/User");
 const app = express();
 const port = 5000;
 
-app.use("/", (req, res) => res.send("Hello World!"));
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "sachin",
+    lastName: "user",
+    email: "sachin@gmail.com",
+    password: "sachin",
+  });
+
+  try {
+    await user.save();
+    res.send("User saved successfully");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 connectDB()
   .then(() => {
