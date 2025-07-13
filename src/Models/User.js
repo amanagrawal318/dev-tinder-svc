df45570d72d3d7dea15db786ff3a3ea13b25ef03
Aman Agrawal <amanagrawal318@gmail.com>
2025-07-13 13:12:01 +0530

    added validator

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -17,10 +18,20 @@ const UserSchema = new mongoose.Schema(
       unique: true,
       trim: true,
       lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid " + value);
+        }
+      },
     },
     password: {
       type: String,
       required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Password should be strong");
+        }
+      },
     },
     age: {
       type: Number,
@@ -42,6 +53,11 @@ const UserSchema = new mongoose.Schema(
     profileUrl: {
       type: String,
       default: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Profile URL is invalid " + value);
+        }
+      },
     },
     skills: {
       type: [String],
