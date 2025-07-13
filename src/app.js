1f9e90be1ea6f0cea3799c04ac44bb0fcf9daed1
Aman Agrawal <amanagrawal318@gmail.com>
2025-07-13 14:14:58 +0530

    added login api

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const connectDB = require("./config/database");
+const validator = require("validator");
 const User = require("./Models/User");
 const validateData = require("./utils/vaildation");
 const app = express();
@@ -9,6 +10,7 @@ const port = 5000;
 
 app.use(express.json());
 
+// POST: Signup API
 app.post("/signup", async (req, res) => {
   try {
     validateData(req.body);
@@ -17,9 +19,7 @@ app.post("/signup", async (req, res) => {
     if (existingUser) {
       throw new Error("User already exists");
     }
-
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const user = new User({
       firstName,
       lastName,
@@ -34,6 +34,31 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// POST: Login API
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      throw new Error("Email and password are required");
+    }
+    if (!validator.isEmail(email)) {
+      throw new Error("Enter Valid Email");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      throw new Error("Invalid credentials");
+    }
+    res.send("User logged in successfully");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// GET: get user
 app.get("/user", async (req, res) => {
   const email = req.body.email;
   try {
@@ -47,6 +72,7 @@ app.get("/user", async (req, res) => {
   }
 });
 
+// GET: get feed
 app.get("/feed", async (req, res) => {
   try {
     const users = await User.find({});
@@ -56,6 +82,7 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+// DELETE: delete user
 app.delete("/user/:userId", async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.userId);
@@ -65,6 +92,7 @@ app.delete("/user/:userId", async (req, res) => {
   }
 });
 
+// PATCH: update user
 app.patch("/user/:userId", async (req, res) => {
   const data = req.body;
   try {
