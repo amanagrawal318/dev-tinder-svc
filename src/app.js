da95f85e403f100d33682c5c03493faaccf71651
Aman Agrawal <amanagrawal318@gmail.com>
2025-07-19 19:30:01 +0530

    removed unused code

--- src/app.js
+++ src/app.js
@@ -25,67 +25,6 @@ app.use("/user", userRouter);
 app.get("/swagger.json", (req, res) => {
   res.json(swaggerDocs);
 });
-/*
-// GET: get feed
-app.get("/feed", userAuth, async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// DELETE: delete user
-app.delete("/user", userAuth, async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.user._id);
-    res.send("user deleted successfully");
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// PATCH: update user
-app.patch("/user", userAuth, async (req, res) => {
-  const data = req.body;
-  try {
-    const ALLOWED_UPDATE_FIELDS = [
-      "firstName",
-      "lastName",
-      "gender",
-      "about",
-      "profileUrl",
-      "skills",
-    ];
-    const isValidOperation = Object.keys(data).every((update) =>
-      ALLOWED_UPDATE_FIELDS.includes(update)
-    );
-    if (!isValidOperation) {
-      throw new Error("Invalid update");
-    }
-    const user = await User.findByIdAndUpdate(req.user._id, data, {
-      runValidators: true,
-    });
-    if (!user) {
-      throw new Error({ message: "User not found" });
-    }
-    res.send("user updated successfully");
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// POST: logout
-app.post("/logout", userAuth, (req, res) => {
-  try {
-    res.clearCookie("token");
-    res.send("User logged out successfully");
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-*/
 
 connectDB()
   .then(() => {
